6e26aae5c77826a29c186179e9da5b0b7d5126b9
Nick Lu <nicklu717@gmail.com>
2025-08-24 08:01:04 +0800

    feat: add common header values

--- Sources/Networking/HTTP/Endpoint/HTTPEndpoint+Components.swift
+++ Sources/Networking/HTTP/Endpoint/HTTPEndpoint+Components.swift
@@ -38,3 +38,41 @@ extension HTTPEndpoint {
         }
     }
 }
+
+// MARK: - Common Header Values
+public extension HTTPEndpoint.HTTPHeader {
+    
+    static func authorization(_ authorization: Authorization) -> Self {
+        Self(.authorization, authorization.value)
+    }
+    enum Authorization {
+        case raw(String)
+        case basic(username: String, password: String)
+        case bearer(token: String)
+        
+        var value: String {
+            switch self {
+            case .raw(let value):
+                return value
+            case .basic(let username, let password):
+                return "Basic \(Data("\(username):\(password)".utf8).base64EncodedString())"
+            case .bearer(let token):
+                return "Bearer \(token)"
+            }
+        }
+    }
+    
+    static func accept(_ contentType: ContentType) -> Self {
+        Self(.accept, contentType.rawValue)
+    }
+    static func contentType(_ contentType: ContentType) -> Self {
+        Self(.contentType, contentType.rawValue)
+    }
+    enum ContentType: String {
+        case json = "application/json"
+        case formURLEncoded = "application/x-www-form-urlencoded"
+        case multipartFormData = "multipart/form-data"
+        case plain = "text/plain"
+        case html = "text/html"
+    }
+}
