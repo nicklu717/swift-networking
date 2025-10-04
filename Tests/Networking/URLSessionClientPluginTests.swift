0651ae2c2d3384e61a42b4c71ea202415583c76b
Nick Lu <nicklu717@gmail.com>
2025-10-04 11:51:30 +0800

    test: URLSessionClient.Plugin

--- Tests/Networking/URLSessionClientPluginTests.swift
+++ Tests/Networking/URLSessionClientPluginTests.swift
@@ -11,41 +11,81 @@ import Foundation
 @testable import Networking
 
 @Suite
-enum URLSessionClientPluginTests {
+struct URLSessionClientPluginTests {
     
+    @Test
+    func success() async {
+        let mockURLRequest = URLRequest(url: URL(string: "Modified")!)
+        let mockPlugin = MockPlugin(mockURLRequest: mockURLRequest)
+        let client = URLSessionClient(urlSession: MockURLSession(mockResult: .success(())), plugins: [mockPlugin])
+        switch await client.requestData(url: URL(string: "Origin")!) {
+        case .success:
+            #expect(mockPlugin.willSendRequest == mockURLRequest)
+            #expect(mockPlugin.didReceiveDataRequest == mockURLRequest)
+            #expect(mockPlugin.didReceiveError == nil)
+        case .failure:
+            #expect(Bool(false))
+        }
+    }
+    
+    @Test
+    func failure() async {
+        let mockError = URLError(.unknown)
+        let mockURLRequest = URLRequest(url: URL(string: "Modified")!)
+        let mockPlugin = MockPlugin(mockURLRequest: mockURLRequest)
+        let client = URLSessionClient(urlSession: MockURLSession(mockResult: .failure(mockError)), plugins: [mockPlugin])
+        switch await client.requestData(url: URL(string: "Origin")!) {
+        case .success:
+            #expect(Bool(false))
+        case .failure:
+            #expect(mockPlugin.willSendRequest == mockURLRequest)
+            #expect(mockPlugin.didReceiveDataRequest == nil)
+            #expect(mockPlugin.didReceiveError == mockError)
+        }
+    }
 }
 
 extension URLSessionClientPluginTests {
     
-    class MockURLSession: URLSessionProtocol {
-        enum TestCase {
-            case success
-            case notHTTPResponse
-            case requestFailure(Int)
-            case urlSessionError(Error)
-            case selfNotExist
-        }
-        private let testCase: TestCase
+    class MockPlugin: URLSessionClient.Plugin {
+        private let mockURLRequest: URLRequest
         
-        init(testCase: TestCase) {
-            self.testCase = testCase
+        private(set) var willSendRequest: URLRequest?
+        private(set) var didReceiveDataRequest: URLRequest?
+        private(set) var didReceiveError: URLError?
+        
+        init(mockURLRequest: URLRequest) {
+            self.mockURLRequest = mockURLRequest
+        }
+        
+        override func modify(request: URLRequest) -> URLRequest {
+            return mockURLRequest
+        }
+        
+        override func willSend(request: URLRequest) {
+            willSendRequest = request
+        }
+        
+        override func didReceive(data: Data, response: URLResponse, request: URLRequest) {
+            didReceiveDataRequest = request
+        }
+        
+        override func didReceive(error: URLError, request: URLRequest) {
+            didReceiveError = error
+        }
+    }
+    
+    class MockURLSession: URLSessionProtocol {
+        private let mockResult: Result<Void, URLError>
+        
+        init(mockResult: Result<Void, URLError>) {
+            self.mockResult = mockResult
         }
         
         func data(for request: URLRequest) async throws -> (Data, URLResponse) {
-            switch testCase {
-            case .success, .selfNotExist:
-                return (Data(), makeHTTPURLResponse(request: request, statusCode: 200))
-            case .notHTTPResponse:
-                return (Data(), URLResponse())
-            case .requestFailure(let statusCode):
-                return (Data(), makeHTTPURLResponse(request: request, statusCode: statusCode))
-            case .urlSessionError(let error):
-                throw error
-            }
-        }
-        
-        private func makeHTTPURLResponse(request: URLRequest, statusCode: Int) -> HTTPURLResponse {
-            return HTTPURLResponse(url: request.url!, statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+            try mockResult
+                .map { (Data(), HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!) }
+                .get()
         }
     }
 }
